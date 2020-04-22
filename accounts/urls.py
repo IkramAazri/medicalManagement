98cdb0218744f09920e486d7cf26f63014c7e020
Nouhaila245 <nouhailaahtitiche09@gmail.com>
2020-04-22 20:56:47 +0100

    template

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,15 @@
-from django.urls import path
+from django.urls import path,re_path
 from django.contrib.auth import views as auth_views
 from . import views
+from django.contrib.auth.views import LogoutView
+from django.views.generic import TemplateView
 
 urlpatterns = [
+    path('', views.index, name='home'),
+    re_path(r'^.*\.html', views.pages, name='pages'),
     path('register/', views.registerPage, name="register"),
     path('login/', views.loginPage, name="login"),
+    path('logout/', LogoutView.as_view(), name="logout"),
 
     path('reset_password/', auth_views.PasswordResetView.as_view(template_name="accounts/password_reset.html"), name="reset_password"),
     path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name="accounts/password_reset_sent.html"), name="password_reset_done"),
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,32 @@
 from django.contrib import messages
 from django.contrib.auth import authenticate, login
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect,get_object_or_404
 from .forms import CreateUserForm
+from django.template import loader
+from django.http import HttpResponse
+
+def index(request):
+    context = {'segment': 'index'}
+    return render(request, "index.html", context)
+
+
+def pages(request):
+    context = {}
+    # All resource paths end in .html.
+    # Pick out the html file name from the url. And load that template.
+    try:
+
+        load_template = request.path.split('/')[-1]
+
+        context['segment'] = load_template
+
+        template = loader.get_template('pages/' + load_template)
+        return HttpResponse(template.render(context, request))
+
+    except:
+
+        template = loader.get_template('pages/error-404.html')
+        return HttpResponse(template.render(context, request))
 
 
 def registerPage(request):
